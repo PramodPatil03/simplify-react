335528e60bc960787a6a895c618c92063411bfc1
PramodPatil03 <pramodpatil190203@gmail.com>
2024-08-15 15:54:23 +0530

    Add: initial files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import {Routes, Route, HashRouter} from 'react-router-dom'
 
-import Home from './components/Home/Home'
 import './App.css'
+import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route exact path='/' element={< Home />} />
-          <Route exact path='/login' element={< Login/>} />
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/login' element={<Login />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
